54c73299bdc2198781bb758fb6363a4493bb9f6d
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-19 01:02:36 +0900

    02주차 6번 문항 퀴즈 풀기

---
+++ week2/02quiz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1,2));
+            Console.WriteLine(Sum(1,2,3));
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
