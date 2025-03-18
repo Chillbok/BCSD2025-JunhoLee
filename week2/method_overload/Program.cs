c4a94f7164116a105b368ec71fbd34daee042941
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-19 01:30:36 +0900

    메소드 오버로딩 구현 실습을 위한 폴더 생성 및 코드 추가

---
+++ week2/method_overload/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace method_overload
+{
+    class Program
+    {
+        static void Main()
+        {
+			Console.WriteLine(Sum(1,2));
+			Console.WriteLine(Sum(1.1f, 3.3f));
+        }
+		public static int Sum(int a, int b)
+		{
+			return a + b;
+		}
+		public static float Sum(float a, float b)
+		{
+			return a + b;
+		}
+    }
+}
