aed0b9c921abd6b9456f7510aa3e4229bc26a97e
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-21 08:49:47 +0900

    2025-03-21, logseq -> obsidian 옮기기 완료
    obsidian에 최적화된 문서 구성을 생각해야 함

---
+++ week2/quiz9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace quiz9
+{
+    class Program
+    {
+        static void Main()
+        {
+            method1("1", "2");
+            method2("2", "1");
+            
+        }
+        void method1(string str1, string str2, int num1 = 0)
+        {
+            Console.WriteLine($"{str1} {str2} {num1}");
+        }
+        void method2(string str1, int num1 = 0, string str2 = "null")
+        {
+            Console.WriteLine($"{str1} {str2} {num1}");
+        }
+    }
+}
