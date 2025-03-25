7ab840e8306bd9f04b85f8c83f5ee143abe09e42
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-26 01:10:20 +0900

    week3 배열 실습 코드 추가

---
+++ week3/자료구조/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace 자료구조
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeArray arrayItems = new MakeArray();
+            
+            //정수 배열 생성
+            int[] numbers = new int[5];
+            arrayItems.ResetNumbers(numbers);
+            arrayItems.PrintItems(numbers);
+
+            //문자열 배열 생성
+            string[] names = {"이준호", "김철수", "김영희"};
+            arrayItems.PrintItems(names);
+
+
+        }
+    }
+
+    class MakeArray
+    {
+        //배열 초기화 메소드
+        public void ResetNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+        }
+
+        //정수 배열 출력 메소드
+        public void PrintItems(int[] x)
+        {
+            Console.WriteLine("정수 배열:");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+
+        //문자열 배열 출력 메소드
+        public void PrintItems(string[] x)
+        {
+            Console.WriteLine("문자열 배열:");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+    }
+}
