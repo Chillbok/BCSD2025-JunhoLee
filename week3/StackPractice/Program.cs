f80dd499947b0a52c3e0ad8cf9fa9692f0c6595b
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-27 01:22:04 +0900

    Stack 실습 파일 생성

---
+++ week3/StackPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace StackPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void PrintLine()
+        {
+            Console.WriteLine("====================");
+        }
+    }
+}
