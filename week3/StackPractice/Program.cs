6eb24a2f2baef8991e3d9ad40a9ec0f881b54c97
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-27 02:28:56 +0900

    Stack 실습 작성중

--- week3/StackPractice/Program.cs
+++ week3/StackPractice/Program.cs
@@ -7,6 +7,18 @@ namespace StackPractice
     {
         static void Main(string[] args)
         {
+            //컬렉션 자료구조는 선언한 변수에 new로 메모리를 할당해야 사용할 수 있다.
+            Stack stack01 = new Stack();
+
+            //스택의 최상위에 새로운 요소 추가
+            stack01.Push("문자열");
+            for (int i = 0; i < 3; i++)
+            {
+                stack01.Push(i);
+            }
+
+            Console.WriteLine($"최상위 요소 삭제하지 않고 반환: {stack01.Peek()}");
+            
         }
 
         public static void PrintLine()
