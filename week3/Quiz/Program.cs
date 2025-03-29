89f5283afcc815cacebd96513fa544385de31e6b
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-30 02:26:35 +0900

    퀴즈 코드 예제 완료

---
+++ week3/Quiz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Quiz
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("문제 1)");
+            quiz1();
+            Console.WriteLine("문제 2)");
+            quiz2();
+        }
+        public static void quiz1()
+        {
+            //ArrayList arr;
+            ArrayList arr = new ArrayList();
+            for(int i = 0;i<=10;i++)
+            {
+                //arr[i] = i;
+                arr.Add(i);
+                Console.WriteLine("nums : " + arr[i]);
+            }
+        }
+        public static void quiz2()
+        {
+            Stack<int> stack = new Stack<int>();
+            Queue<int> queue = new Queue<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            queue.Enqueue(stack.Pop());
+            stack.Push(queue.Dequeue());
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
