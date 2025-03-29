fa90ff3a2b2c0a32fd1074433d5b0a214b47fa99
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-29 17:43:15 +0900

    Hashtable 실습 완료

--- week3/HashtablePractice/Program.cs
+++ week3/HashtablePractice/Program.cs
@@ -18,14 +18,42 @@ namespace HashtablePractice
             hash["Goblin"] = goblin;
 
             //요소 추가(Add)
-            hash.Add("Slime", slime);
-            hash.Add(1, "정수");
-            hash.Add(1.2f, "실수");
-            hash.Add("안녕하세요. 이준호입니다.", "문자열")
+            hash.Add("class", slime);
+            hash.Add("정수", 10);
+            hash.Add("실수", 1.2f);
+            hash.Add("문자열", "안녕하세요 이준호입니다");
+            Console.WriteLine("요소 추가 후 리스트:");
+            PrintHashtable(hash);
 
-            //
-            
+            //요소 삭제
+            hash.Add("삭제용 키", "삭제용 값");
+            PrintLine();
+            Console.WriteLine("삭제할 요소 추가 후 내용:");
+            PrintHashtable(hash);
+            PrintLine();
+            Console.WriteLine("요소 삭제 후 내용:");
 
+            //키, 값 확인
+            Console.WriteLine($"\t\"삭제용 키\"가 key인 요소 있는지 확인: {hash.ContainsKey("삭제용 키")}");
+            Console.WriteLine($"\t\"삭제용 값\"이 value인 요소 있는지 확인: {hash.ContainsValue("삭제용 값")}");
+            hash.Remove("삭제용 키");
+            PrintHashtable(hash);
+            hash.Remove("삭제용 키");
+
+            //요소 개수 확인
+            PrintLine();
+            Console.WriteLine($"\"hash\" 해시테이블의 키 개수: {hash.Count}");
+            Console.WriteLine("\"hash\" 해시테이블의 키:");
+            foreach(string key in hash.Keys)
+            {
+                Console.WriteLine($"\t{key}");
+            }
+
+            //해시테이블 초기화
+            PrintLine();
+            Console.WriteLine($"해시테이블 초기화 후 해시테이블 요소:");
+            PrintHashtable(hash);
+            Console.WriteLine($"해시테이블 초기화 후 해시테이블 요소 개수: {hash.Count}");
         }
 
         public static void PrintLine()
@@ -37,21 +65,22 @@ namespace HashtablePractice
         {
             foreach(object key in x.Keys)
             {
-                Console.WriteLine($"hash[{key}] = {hash[key]}");
+                Console.WriteLine($"\tx[{key}] = {x[key]}");
             }
         }
     }
 
-    class Goblin()
+    class Goblin
     {
 
     }
 
-    class Slime()
+    class Slime
     {
 
     }
 }
+/*
 foreach (var arr in array);
 {
     Console.WriteLine(arr);
@@ -62,3 +91,4 @@ foreach(var key in hash.Keys)
 {
     Console.WriteLine($"key: {key}, Value: {hash[key]}");
 }
+*/
